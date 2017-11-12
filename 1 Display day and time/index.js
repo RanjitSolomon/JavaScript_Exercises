c26b5aca441f05eec65e8ae3e4ed6a237950d0b4
rsolo__ <ranjitsolo@gmail.com>
2017-11-11 23:44:12 -0500

    First exercise

---
+++ 1 Display day and time/index.js
@@ -0,0 +1,8 @@
+function displayCurrentDayAndTime(){
+        let today = new Date();
+        let day = today.getDay();
+        let dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        document.getElementById("displayCurrentDay").innerHTML = "Today is : " + dayList[day]+ ".";
+
+        document.getElementById("displayCurrentTime").innerHTML = `Current Time is : ${today.getHours() >= 12 ? (today.getHours()-12):(today.getHours())} ${today.getHours() >=12 ? "PM":"AM"} : ${today.getMinutes()} : ${today.getSeconds()}`;
+}
